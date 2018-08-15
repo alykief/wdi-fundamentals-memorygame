dfad4160852b8dc24b0a824618acc997dfa1afe4
Alyssa Colette Kieffer <ack.photography@aol.com>
2018-08-15 15:32:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,34 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//Create an array to store cards
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardOne);
+//Create an empty array 
+var cardsInPlay = [];
+
+//Create a variable cardOne
+var cardOne = cards[0];
+
+//Add first card to cardsInPlay array
+cardsInPlay.push(cardOne);
+
+//Use console.log() to display the User flipped card
+console.log("User flipped" + cardOne);
+
+//Create a variable cardTwo
+var cardTwo = cards[2];
+
+//Add second card to cardsInPlay array
+cardsInPlay.push(cardTwo);
+
+//Use console.log() to display the User flipped card 
+console.log("User flipped" + cardTwo);
+
+//Write an if statement to check if the length of cardsInPlay array is 2
+if (cardsInPlay.length === 2) {
+	//Write an if...else statement that checks for equality of the two cards
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		//If values are equal, alert "Match", otherwise, alert "Try again"
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+		}
+	}
