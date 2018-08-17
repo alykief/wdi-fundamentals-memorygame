da4397be989213b1f7059c2b59a5ffa8667cbb88
Alyssa Colette Kieffer <ack.photography@aol.com>
2018-08-17 15:39:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,6 +27,7 @@ var cardsInPlay = [];
 
 //Create a function to check for match
 var checkForMatch = function () {
+	//Check cards in play and make pop-up for user reference
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
@@ -35,20 +36,38 @@ var checkForMatch = function () {
 };
 
 //Create a function called flipCard that accepts one parameter
-var flipCard = function (cardId) {
-		//Add a console.log() statement to display flipped card
-		console.log("User flipped " + cards[cardId].card);
+var flipCard = function () {
+	//Store card that was just flipped by user 
+	cardId = this.getAttribute('data-id');
+		console.log(cardId);
 		//Add the card that the user flipped to the cardsInPlay array
 		cardsInPlay.push(cards[cardId].card);
 		//Create console.log() for image and suit
 		console.log(cards[cardId].cardImage)
 		console.log(cards[cardId].suit)
-		//Check if cards have been played 
-		if (cardsInPlay.length === 2) {
-			checkForMatch();
-			}
+	//Update the src to image of card clicked 
+	this.setAttribute('src', cards[cardId].cardImage);
+	//Check if cards have been played 
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		}
 };
 
-//Simulate user flipping a card 
-flipCard(0);
-flipCard(2);
+//Write a function to create a new game board
+var createBoard = function () {
+	//Loop through the cards array 
+	for (var i = 0; i < cards.length; i++){
+		//Create img element and store in cardElement 
+		var cardElement = document.createElement('img');
+			//Use setAttribute() to add a src so users see back of card 
+			cardElement.setAttribute('src', 'images/back.png');
+			//Set card's data-id to current index within array 
+			cardElement.setAttribute('data-id', i);
+			//Add a click event to flip the card 
+			cardElement.addEventListener('click', flipCard);
+			//Append cardElement to game-board
+			document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+//Invoke createBoard function
+createBoard();
